4a899559a6703b1eb4734a63b5edd23c295dc463
khbarshasingha <48600202+khbarshasingha@users.noreply.github.com>
2019-03-29 03:43:26 +0530

    Add files via upload

---
+++ fractionnn.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+struct fraction{
+	int num;
+	int den;
+	
+};
+
+
+fraction add(int n , struct fraction arr[])
+{
+	int i;
+	fraction x;
+	x.den=1, x.num=0;
+	for(i=0;i<n;i++)
+	{
+		x.den=x.den * arr[i].den ;
+	}
+	for(i=0;i<n;i++)
+	{
+		x.num+=((x.den/arr[i].den) * arr[i].num );
+	}
+	
+	return x;
+}
+int main()
+{
+	struct fraction arr[100],z;
+	int n, i;
+	cin>>n;
+	for(i=0;i<n;i++)
+	{
+		cin>>arr[i].num>>arr[i].den;
+	}
+
+	z=add(n,arr);
+	cout<<z.num<<"/"<<z.den;
+	return 0;
+}
