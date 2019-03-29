f9695f61556e05641f3a43ceaa042f74c9c7a1f0
khbarshasingha <48600202+khbarshasingha@users.noreply.github.com>
2019-03-29 10:30:01 +0530

    Update fractionnn.cpp

--- fractionnn.cpp
+++ fractionnn.cpp
@@ -8,16 +8,18 @@ struct fraction{
 	
 };
 
-
+//to calculate the sum of n numbers of fractions
 fraction add(int n , struct fraction arr[])
 {
 	int i;
 	fraction x;
 	x.den=1, x.num=0;
+	//calculation of denominator
 	for(i=0;i<n;i++)
 	{
 		x.den=x.den * arr[i].den ;
 	}
+	//calculation of numerator
 	for(i=0;i<n;i++)
 	{
 		x.num+=((x.den/arr[i].den) * arr[i].num );
